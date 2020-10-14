047c62b98b03bb23447a872aa3cb9b18a7066361
daniel <duque1daniel@gmail.com>
2020-10-14 13:03:36 -0700

    add user input/print statements

--- LocalTube.py
+++ LocalTube.py
@@ -1,1 +1,20 @@
 from pytube import YouTube
+
+# Handles user input
+link = input("Enter YT video link: ")
+yt = YouTube(link)
+
+# Video Title
+print("Title of video: ",yt.title)
+
+# Number of views of video
+print("Number of views: ",yt.views)
+
+# Length of video
+print("Length of video: ",yt.length, "seconds")
+
+# Description of video
+print("Description: ",yt.description)
+
+# Video Rating
+print("Ratings: ",yt.rating)
