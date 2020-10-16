db0c3d41d3ff6857aa0a259ee94d38511a5aec8d
daniel <duque1daniel@gmail.com>
2020-10-16 11:28:17 -0700

    added quality label/selectors/dlbtn

--- LocalTube.py
+++ LocalTube.py
@@ -9,7 +9,7 @@ root.geometry("350x350")
 root.columnconfigure(0, weight=1)
 
 #Link label
-LinkLabel =  Label(root, text="Enter YouTube URL below!", font=("Helvetica", 15))
+LinkLabel =  Label(root, text="Enter YouTube URL below!", font=("Helvetica",15,"bold"))
 LinkLabel.grid()
 
 # Link input
@@ -18,11 +18,11 @@ LinkInput = Entry(root,width=50,textvariable=LinkInputVar)
 LinkInput.grid()
 
 # Error msg if unknown link is used
-LinkError = Label(root,text="error unknown link",fg="red",font=("Helvetica", 10))
+LinkError = Label(root,text="error unknown link",fg="red",font=("Helvetica",10,"bold"))
 LinkError.grid()
 
 # Video save options label
-SaveLabel = Label(root,text="Video save options:",font=("Helvetica", 15))
+SaveLabel = Label(root,text="Video save options:",font=("Helvetica",15,"bold"))
 SaveLabel.grid()
 
 # File path browse button
@@ -32,5 +32,18 @@ SaveInput.grid()
 # Error msg if no path selected
 PathError = Label(root,text="error select download location", fg="red", font=("Helvetica",10))
 PathError.grid()
+
+# Quality label
+QualitySelect = Label(root,text="Select download quality",font=("Helvetica",15,"bold"))
+QualitySelect.grid()
+
+# Quality choices
+qualityChoice = ["1080p - (mp4)","720p - (mp4)","480p - (mp4)","144p - (mp4)","Audio -  (mp3)"]
+QualityChoices = ttk.Combobox(root,values=qualityChoice)
+QualityChoices.grid()
+
+# Download button
+downloadbtn = Button(root,text="Download",width=10,bg="red",fg="white")
+downloadbtn.grid()
 
 root.mainloop()
