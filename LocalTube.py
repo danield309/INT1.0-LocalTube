f62d1082b02b25dc0d8380bd23a7fad5567fe38d
daniel <duque1daniel@gmail.com>
2020-10-16 16:35:13 -0700

    added file path/download functions

--- LocalTube.py
+++ LocalTube.py
@@ -3,13 +3,51 @@ from tkinter import ttk
 from tkinter import filedialog
 from pytube import YouTube
 
+Folder_Name = ""
+
+# File Location
+def openLocation():
+    global Folder_Name
+    Folder_Name = filedialog.askdirectory()
+    if(len(Folder_Name) > 1):
+        PathError.config(text=Folder_Name,fg="blue")
+    else:
+        PathError.config(text="Error select a file location",fg="black",bg="red")
+
+#donwload video
+def DownloadVideo():
+    choice = qualityChoices.get()
+    url = LinkInput.get()
+
+    if(len(url)>1):
+        LinkError.config(text="")
+        yt = YouTube(url)
+
+        if(choice == choices[0]):
+            select = yt.streams.filter(progressive=True).first()
+
+        elif(choice == choices[1]):
+            select = yt.streams.filter(progressive=True,file_extension='mp4').last()
+
+        elif(choice == choices[2]):
+            select = yt.streams.filter(only_audio=True).first()
+
+        else:
+           LinkError.config(text="Paste Link again!!",fg="red")
+
+
+    #download function
+    select.download(Folder_Name)
+    LinkError.config(text="Download Completed!!")
+
 root = Tk()
 root.title("LocalTube")
 root.geometry("350x350")
+root.configure(bg='red')
 root.columnconfigure(0, weight=1)
 
 #Link label
-LinkLabel =  Label(root, text="Enter YouTube URL below!", font=("Helvetica",15,"bold"))
+LinkLabel =  Label(root, text="Enter YouTube URL below!",bg="red",font=("Helvetica",15,"bold"))
 LinkLabel.grid()
 
 # Link input
@@ -18,32 +56,36 @@ LinkInput = Entry(root,width=50,textvariable=LinkInputVar)
 LinkInput.grid()
 
 # Error msg if unknown link is used
-LinkError = Label(root,text="error unknown link",fg="red",font=("Helvetica",10,"bold"))
+LinkError = Label(root,text="error unknown link",fg="black",bg="red",font=("Helvetica",10,"bold"))
 LinkError.grid()
 
 # Video save options label
-SaveLabel = Label(root,text="Video save options:",font=("Helvetica",15,"bold"))
+SaveLabel = Label(root,text="Video save options:",bg="red",font=("Helvetica",15,"bold"))
 SaveLabel.grid()
 
 # File path browse button
-SaveInput = Button(root,width=20,bg="blue",fg="white", text="Select Download Location")
+SaveInput = Button(root,width=20,text="Select Download Location",bg="red",command=openLocation)
 SaveInput.grid()
 
 # Error msg if no path selected
-PathError = Label(root,text="error select download location", fg="red", font=("Helvetica",10))
+PathError = Label(root,text="error select download location", fg="black",bg="red", font=("Helvetica",10,"bold"))
 PathError.grid()
 
 # Quality label
-QualitySelect = Label(root,text="Select download quality",font=("Helvetica",15,"bold"))
+QualitySelect = Label(root,text="Select download quality",bg="red",font=("Helvetica",15,"bold"))
 QualitySelect.grid()
 
-# Quality choices
-qualityChoice = ["1080p - (mp4)","720p - (mp4)","480p - (mp4)","144p - (mp4)","Audio -  (mp3)"]
-QualityChoices = ttk.Combobox(root,values=qualityChoice)
-QualityChoices.grid()
+#combobox
+choices = ["720p","144p","Only Audio"]
+qualityChoices = ttk.Combobox(root,values=choices)
+qualityChoices.grid()
 
 # Download button
-downloadbtn = Button(root,text="Download",width=10,bg="red",fg="white")
+downloadbtn = Button(root,text="Download",width=10,fg="red",command=DownloadVideo)
 downloadbtn.grid()
+
+# Dev Info
+devLabel = Label(root,bg="red",text="Made by Daniel Duque using Python & Tkinter",font=("Helvetica",15,"bold"))
+devLabel.grid()
 
 root.mainloop()
