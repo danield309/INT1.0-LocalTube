de30b30f111db24c1f384fb08f759b0828cece72
daniel <duque1daniel@gmail.com>
2020-10-16 10:51:54 -0700

    added link label and input

--- LocalTube.py
+++ LocalTube.py
@@ -1,22 +1,24 @@
-from pytube import YouTube
 from tkinter import *
-
-class LocalTubeGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("LocalTube")
-
-        Label(master, text="LocalTube").pack()
-
-        # Enter youtube video URL here
-
-        # Dropdown menu option
-
-        # Download after selecting menu option
-
-        Button(master, text="Exit Program", command=master.quit).pack()
+from tkinter import ttk
+from tkinter import filedialog
+from pytube import YouTube
 
 root = Tk()
-root.geometry("1000x700")
-my_gui = LocalTubeGUI(root)
+root.title("LocalTube")
+root.geometry("350x350")
+root.columnconfigure(0, weight=1)
+
+#Link Label
+LinkLabel =  Label(root, text="Enter YouTube URL below!", font=("Helvetica", 15))
+LinkLabel.grid()
+
+# Link Input
+LinkInputVar = StringVar()
+LinkInput = Entry(root,width=50,textvariable=LinkInputVar)
+LinkInput.grid()
+
+#Error msg if unknown link is used
+LinkError = Label(root,text="Error",fg="red",font=("Helvetica", 10))
+LinkError.grid()
+
 root.mainloop()
