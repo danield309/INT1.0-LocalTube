bd7d87032ff8f69649d840ee7d78f0f607dd1f7c
daniel <duque1daniel@gmail.com>
2020-10-16 11:04:38 -0700

    added file path label/selector

--- LocalTube.py
+++ LocalTube.py
@@ -8,17 +8,29 @@ root.title("LocalTube")
 root.geometry("350x350")
 root.columnconfigure(0, weight=1)
 
-#Link Label
+#Link label
 LinkLabel =  Label(root, text="Enter YouTube URL below!", font=("Helvetica", 15))
 LinkLabel.grid()
 
-# Link Input
+# Link input
 LinkInputVar = StringVar()
 LinkInput = Entry(root,width=50,textvariable=LinkInputVar)
 LinkInput.grid()
 
-#Error msg if unknown link is used
-LinkError = Label(root,text="Error",fg="red",font=("Helvetica", 10))
+# Error msg if unknown link is used
+LinkError = Label(root,text="error unknown link",fg="red",font=("Helvetica", 10))
 LinkError.grid()
+
+# Video save options label
+SaveLabel = Label(root,text="Video save options:",font=("Helvetica", 15))
+SaveLabel.grid()
+
+# File path browse button
+SaveInput = Button(root,width=20,bg="blue",fg="white", text="Select Download Location")
+SaveInput.grid()
+
+# Error msg if no path selected
+PathError = Label(root,text="error select download location", fg="red", font=("Helvetica",10))
+PathError.grid()
 
 root.mainloop()
